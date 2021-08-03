7635f504cd785c624296a7f9c9ffa193d1a06f84
Alexandre CARVUNIS <carvunis@lapthui7d.in2p3.fr>
2021-08-03 13:09:34 +0200

    minor changes

--- smelli/classes.py
+++ smelli/classes.py
@@ -62,6 +62,7 @@ class GlobalLikelihood(object):
         'fast_likelihood_BDstarlv_fixckm_Belle_only.yaml',
         'fast_likelihood_BDstarlv_fixckm_w_DeltaAFB.yaml',
         'fast_likelihood_BDstarlv_fixckm_bobeth_ang.yaml',
+        'fast_likelihood_BDstarlv_fixckm_bobeth_ang_Delta.yaml',
         'fast_likelihood_bsll_fixCKM.yaml',
         'fast_likelihood_Belle2017.yaml'
     ]
